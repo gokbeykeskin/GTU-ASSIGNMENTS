8c6618292af7bd995bf7e3c8c0d44c4678daba03
gokbeykeskin <gokbeykeskin@gmail.com>
2021-05-08 02:46:19 +0300

    Javadocs Removed
    Javadoc files are removed and added to makefile. command make will compile,create javadoc and run.

--- CSE222/HW4/JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"MyContainers","l":"BSTHeapTree"},{"p":"MyContainers","l":"MyHeap"},{"p":"MyContainers","l":"MyIterator"},{"p":"MyContainers","l":"Test"}]
